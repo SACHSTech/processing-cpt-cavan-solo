2f49980aede8dde6db0718c325ac078d3e54e017
Cavan Chung <75390863+CavanChung@users.noreply.github.com>
2022-06-16 02:46:19 +0000

    Added collision detection between wall and car

--- Sketch.java
+++ Sketch.java
@@ -1,5 +1,7 @@
 import processing.core.PApplet;
 import processing.core.PImage;
+import java.util.ArrayList;
+
 public class Sketch extends PApplet {
 
   PImage imgSetting1;
@@ -7,6 +9,7 @@ public class Sketch extends PApplet {
   PImage imgSetting3;
   PImage imgPlayer;
   PImage imgWall;
+  
   int intTimeCalc = 0;
   int intTime = 0;
   int intProjectileX;
@@ -14,17 +17,21 @@ public class Sketch extends PApplet {
   int intWallSpeed = 2;
   int intWallOneX = 0;
   int intWallTwoX = 0;
-  float fltCarX = 200;
-  float fltCarY = 200;
+  int intHealth = 100;
+
+  
+  float fltCarX = 700;
+  float fltCarY = 300;
   
 
-  boolean mainMenu = true;
+  boolean blnMainMenu = true;
   boolean blnGame1 = false;
   boolean blnGame2 = false;
   boolean blnGame3 = false;
   boolean blnGameStart = false;
-  boolean blnChaosDifficulty = false;
+  boolean blnNitro = false;
 
+  ArrayList<Integer> intHitCount = new ArrayList<Integer>();
   
   public void settings() {
     size(800, 400);
@@ -53,22 +60,22 @@ public class Sketch extends PApplet {
 
   public void draw() {
 
-    if (mainMenu == true) {
+    if (blnMainMenu == true) {
       // main menu screen
     }
 
-    if (keyPressed && key == '1' && mainMenu == true) {
-      mainMenu = false;
+    if (keyPressed && key == '1' && blnMainMenu == true) {
+      blnMainMenu = false;
       blnGame1 = true;
     }
 
-    if (keyPressed && key == '2' && mainMenu == true) {
-      mainMenu = false;
+    if (keyPressed && key == '2' && blnMainMenu == true) {
+      blnMainMenu = false;
       blnGame2 = true;
     }
 
-    if (keyPressed && key == '3' && mainMenu == true) {
-      mainMenu = false;
+    if (keyPressed && key == '3' && blnMainMenu == true) {
+      blnMainMenu = false;
       blnGame3 = true;
     }
     
@@ -91,7 +98,8 @@ public class Sketch extends PApplet {
       image(imgSetting3, 0, 0);
     }
 
-    if (blnGameStart == true){
+  //game start
+  if (blnGameStart == true && intHealth > 0){
 
     // not exact seconds but close enough
     intTimeCalc += 2;
@@ -99,6 +107,7 @@ public class Sketch extends PApplet {
     // display time
     fill(0);
     text("time: " + intTime, width/1.5f, height/3.8f);
+    text("health: " + intHealth, width/2.2f, height/3.8f);
 
     //player stuff
     image(imgPlayer, fltCarX, fltCarY);
@@ -144,19 +153,22 @@ public class Sketch extends PApplet {
     }
 
     // All of the projectiles on the road
-    if (intTime > 60 && intTime <= 90){
+    if (intTime > 60){
       
     }
 
-    // Boss
-    if (blnChaosDifficulty == true && intTime >= 90) {
-      
-    }
-    
-    
+    //collision detection wall
+    if (dist(fltCarX, fltCarY, intWallOneX, height/1.69f) <= 37.4){
+        intHealth -= 1;
+      }
+    if (dist(fltCarX, fltCarY, intWallTwoX, height/1.26f) <= 37.4){
+        intHealth -= 1;
+      }
 
       
-    }
+  }
+
+    
   } 
   public void keyPressed() {
     if (key == 'w') {
